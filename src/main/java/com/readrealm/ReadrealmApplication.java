bd5e408f7b9cbf28f4bd40bb8d9a9280e942eff8
Andrezza Medeiros Souza <andrezzaammss@gmail.com>
2024-01-16 00:09:15 -0300

    First commit

---
+++ src/main/java/com/readrealm/ReadrealmApplication.java
@@ -0,0 +1,13 @@
+package com.readrealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadrealmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadrealmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readrealm/ReadrealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readrealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadrealmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
